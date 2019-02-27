dcbaee956d612ef3766104715f18c9bb4c8cf3a0
ginkaikei <ginkaikei@gmail.com>
2019-02-27 14:46:37 -0800

    got a taste of testing with mocha

--- test/utils/ultis.test.js
+++ test/utils/ultis.test.js
@@ -8,10 +8,33 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number')
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  })
+});
+
 it('should square a number', () => {
   var res = utils.square(3);
 
   expect(res).toBe(9).toBeA('number')
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(4, (sum) => {
+    expect(sum).toBe(16).toBeA('number');
+    done();
+  });
+});
+
+it('should be my name', () => {
+  var user = { location: "CA", age: 25 };
+  var res = utils.setName(user, 'Daniel Nguyen');
+  expect(user).toInclude({
+    firstName: 'Daniel',
+    lastName: 'Nguyen'
+  });
 });
 
 // it('should expect some values', () => {
