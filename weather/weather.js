0a23843f19505d9f32739694cfc0cb8ba251a780
Daniel Nguyen <danqnguyen12@gmail.com>
2019-04-09 00:01:38 -0700

    Update weather.js
    removed api key

--- weather/weather.js
+++ weather/weather.js
@@ -3,7 +3,7 @@ const request = require('request');
 var getWeather = (lat, long, callback) => {
 
   request({
-    url: `https://api.darksky.net/forecast/a9ff63399803eebd58cec33e6183be5d/${lat},${long}`,
+    url: `https://api.darksky.net/forecast/apikeyhere/${lat},${long}`,
     json: true
   }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
